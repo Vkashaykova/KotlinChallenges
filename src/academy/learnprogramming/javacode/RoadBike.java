c4684ae0f15e5020b405429497d7e7a98b2e981b
Vkashaykova <vkashaykova@gmail.com>
2024-06-06 10:42:04 +0300

    Initial commit

---
+++ src/academy/learnprogramming/javacode/RoadBike.java
@@ -0,0 +1,27 @@
+package academy.learnprogramming.javacode;
+
+import academy.learnprogramming.javacode.Bicycle;
+
+public class RoadBike extends Bicycle {
+
+    // In millimetres
+    private int tireWidth;
+
+    public RoadBike(int cadence,
+                    int speed,
+                    int gear,
+                    int tireWidth) {
+        super(cadence, speed, gear);
+        this.tireWidth = tireWidth;
+    }
+
+    public int getTireWidth() {
+        return tireWidth;
+    }
+
+    public void printDescription() {
+        super.printDescription();
+        System.out.println("The road bike has a tire width of " + tireWidth + " MM.");
+    }
+
+}
